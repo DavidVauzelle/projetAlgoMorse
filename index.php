6660776ed4ab2ff788e6cc99ada5fd7d97886166
David Vauzelle <dvauzelle@deastanceservices.fr>
2024-11-13 09:10:13 +0100

    Premier push projetAlgo.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Convertisseur Morse</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <main>
+        <form class="" action="">
+            <div>
+                <textarea name="" id=""></textarea>
+                <button>Convertir en texte</button>    
+            </div>
+            <div>
+                <textarea name="" id=""></textarea>
+                <button>Convertir en Morse</button>        
+            </div>
+        </form>
+    </main>    
+</body>
+</html>
